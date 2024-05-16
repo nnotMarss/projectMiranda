e99875581a7f6038cf3224cf7a37793049e42110
m4rs_fox <m4rs_fox@outlook.com.au>
2024-05-16 17:11:08 +0200

    {TRANSFER] PyCharm

--- _system_.fld/lib/miranda_log/__init__.py
+++ _system_.fld/lib/miranda_log/__init__.py
@@ -19,11 +19,14 @@ def log(action, description):
         file.write(msg)
 
 def clear(d=False):
+    idPath = os.path.join(miranda_folder.root(), "_system_.fld", "var", "ID.var")
+    id = int(open(idPath, "r").read())
     path = os.path.join(miranda_folder.root(), "_logs_.fld")
     pattern = re.compile(r"_log_ID\d+_.log$")
     for filename in os.listdir(path):
-        if pattern.match(filename):
+        if pattern.match(filename) and filename != f"_log_ID{id}_.log":
             fPath = os.path.join(path, filename)
             os.remove(fPath)
+            log("INFO", "DELETED FILE: %s" % fPath)
             if d:
                 print(":: REMOVED FILE: %s" % fPath) 
--- _system_.fld/lib/terra_lang/__init__.py
+++ _system_.fld/lib/terra_lang/__init__.py
@@ -26,109 +26,31 @@ def Decode(inputText=""):
         inputText = inputText.replace(key, value)
     return inputText
 
-class Lingo:
-    class LetterA:
-        Disp = "A"
-        Sound = "Fa"
-    class LetterB:
-        Disp = "B"
-        Sound = "Fe"
-    class LetterC:
-        Disp = "C"
-        Sound = "Fu"
-    class LetterD:
-        Disp = "D"
-        Sound = "Ja"
-    class LetterE:
-        Disp = "E"
-        Sound = "Je"
-    class LetterF:
-        Disp = "F"
-        Sound = "Ju"
-    class LetterG:
-        Disp = "G"
-        Sound = "Ka"
-    class LetterH:
-        Disp = "H"
-        Sound = "Ke"
-    class LetterI:
-        Disp = "I"
-        Sound = "Ku"
-    class LetterJ:
-        Disp = "J"
-        Sound = "Na"
-    class LetterK:
-        Disp = "K"
-        Sound = "Ne"
-    class LetterL:
-        Disp = "L"
-        Sound = "Nu"
-    class LetterM:
-        Disp = "M"
-        Sound = "Ra"
-    class LetterN:
-        Disp = "N"
-        Sound = "Re"
-    class LetterO:
-        Disp = "O"
-        Sound = "Ru"
-    class LetterP:
-        Disp = "P"
-        Sound = "Sa"
-    class LetterQ:
-        Disp = "Q"
-        Sound = "Se"
-    class LetterR:
-        Disp = "R"
-        Sound = "Su"
-    class LetterS:
-        Disp = "S"
-        Sound = "Ta"
-    class LetterT:
-        Disp = "T"
-        Sound = "Te"
-    class LetterU:
-        Disp = "U"
-        Sound = "Tu"
-    class LetterV:
-        Disp = "V"
-        Sound = "Za"
-    class LetterW:
-        Disp = "W"
-        Sound = "Ze"
-    class LetterX:
-        Disp = "X"
-        Sound = "Zu"
-    class LetterY:
-        Disp = "Y"
-        Sound = "Ga"
-    class LetterZ:
-        Disp = "Z"
-        Sound = "Ge"
-    Map = {
-    1: LetterA,
-    2: LetterB,
-    3: LetterC,
-    4: LetterD,
-    5: LetterE,
-    6: LetterF,
-    7: LetterG,
-    8: LetterH,
-    9: LetterI,
-    10: LetterJ,
-    11: LetterK,
-    12: LetterL,
-    13: LetterM,
-    14: LetterN,
-    15: LetterO,
-    16: LetterP,
-    17: LetterQ,
-    18: LetterR,
-    19: LetterS,
-    20: LetterT,
-    21: LetterU,
-    22: LetterV,
-    23: LetterW,
-    24: LetterX,
-    25: LetterY,
-    26: LetterZ}
+
+def Code2(inputText=""):
+    replacements = {
+        "f": "(", "a": "=", "r": ",", "m": "$", "n": "\"", "j": "'", 
+        "e": "*", "k": "#", "b": "~", "h": "}", "t": ".", "d": "/", 
+        "g": "!", "s": "_", "q": "&", "p": "?", "u": ";", "z": "{", 
+        "v": "-", "w": ")", "x": "`", "y": "+", "c": "^", "i": ":", 
+        "o": "°", "l": "@"
+    }
+
+    inputText = inputText.lower()
+    for key, value in replacements.items():
+        inputText = inputText.replace(key, value)
+    return inputText
+
+def Decode2(inputText=""):
+    replacements = {
+        "(": "f", "=": "a", ",": "r", "$": "m", "\"": "n", "'": "j",
+        "*": "e", "#": "k", "~": "b", "}": "h", ".": "t", "/": "d",
+        "!": "g", "_": "s", "&": "q", "?": "p", ";": "u", "{": "z",
+        "-": "v", ")": "w", "`": "x", "+": "y", "^": "c", ":": "i",
+        "°": "o", "@": "l"
+    }
+
+    inputText = inputText.lower()
+    for key, value in replacements.items():
+        inputText = inputText.replace(key, value)
+    return inputText
